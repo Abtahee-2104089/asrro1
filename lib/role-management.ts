9080924339da198d107fde4d7344d70c8a4d34a9
Abtahee-2104089 <u2104089@student.cuet.ac.bd>
2026-08-22 03:17:45 +0600

    Modified role-management.ts to add all roles

--- lib/role-management.ts
+++ lib/role-management.ts
@@ -4,21 +4,28 @@ import type { api } from "@/convex/_generated/api"
 
 export const positionOptions = [
   "president",
-  "vice_president",
+  "vice_president (admin)",
+  "vice_president (logistics)",
+  "vice_president (technical)",
+  "vice_president (r&d)",
   "general_secretary",
   "joint_general_secretary",
   "organizing_secretary",
-  "financial_secretary",
-  "public_relations_secretary",
+  "joint_organizing_secretary",
+  "finance_secretary",
+  "joint_finance_secretary",
+  "robotics_secretary",
+  "space_astronomy_secretary",
   "research_publication_secretary",
-  "technical_secretary",
+  "joint_research_publication_secretary",
+  "programming_software_secretary",
+  "joint_programming_software_secretary",
   "office_secretary",
-  "education_secretary",
-  "publication_secretary",
-  "it_secretary",
-  "event_coordinator",
-  "membership_coordinator",
-  "executive_member",
+  "event_management_secretary",
+  "graphics_design_secretary",
+  "media_secretary",
+  "outreach_sponsor_management_secretary",
+  "executive_member"
 ] as const
 
 export type ExecutivePosition = (typeof positionOptions)[number]
